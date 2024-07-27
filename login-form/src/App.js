e0d4d1ea12480b08eba22dc55d5168781cccc583
Nanawarenishaa <164838299+Nanawarenishaa@users.noreply.github.com>
2024-07-27 23:15:11 +0530

    Add files via upload

---
+++ login-form/src/App.js
@@ -0,0 +1,12 @@
+
+import LoginForm from './Components/Auth/Login';
+import './index.css';
+function App() {
+  return (
+    <div className="App">
+     <LoginForm />
+    </div>
+  );
+}
+
+export default App;
