41e7ed13afbd1c01853a1b41da544faede4526a0
Nanawarenishaa <164838299+Nanawarenishaa@users.noreply.github.com>
2025-01-25 20:03:47 +0530

    Add files via upload

---
+++ SRAAS-Project/src/App.jsx
@@ -0,0 +1,17 @@
+
+import './index.css';
+
+function App() {
+
+
+  return (
+    <>
+      <div className='h-screen w-full bg-slate-900 '>
+       <h1 className="text-white grid place-content-center items-center text-5xl ">hello there! so what</h1>
+      </div>
+    
+    </>
+  )
+}
+
+export default App
